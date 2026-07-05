c8e2da4313325fbc3df62adb18ca4b9f171d96b5
Nguyen Pham Quang Phuong <phuongga84.kfc@gmail.com>
2026-07-05 13:46:53 +0700

    test2

--- backend/src/libs/database.js
+++ backend/src/libs/database.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
+    console.log("Mongo URL:", process.env.MONGODB_URL);
     await mongoose.connect(process.env.MONGODB_URL);
     console.log("Kết nối tới CSDL thành công!");
-    console.log("Mongo URL:", process.env.MONGODB_URL);
   } catch (error) {
     console.log("Lỗi khi kết nối tới CSDL: ", error);
     process.exit(1)
